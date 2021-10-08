6bee0a7e3fe13749ce1c079da1097da64129ff7a
Parasolka <Parasolka@DESKTOP-Q9Q8JLF>
2021-10-08 02:10:54 +0200

    Добавьте файлы проекта.

---
+++ Hw2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Hw2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            {
+                try
+                {
+                    double x, a, b;
+                    Console.Write("Введите a:");
+                    a = double.Parse(Console.ReadLine());
+                    Console.Write("Введите b:");
+                    b = double.Parse(Console.ReadLine());
+                    if (a != 0)
+                    {
+                        x = (12 - b * a) / 2;
+                        Console.WriteLine("x=" + x);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Нет решения");
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Введите число, а не букву");
+                }
+            }
+            
+        }
+    }
+}
